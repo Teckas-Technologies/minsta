6277370ef2cea6b18808048bb7d57599eb08bdf2
sathish <sathish@kalakendradao.org>
2024-06-20 15:25:11 +0530

    Updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -131,7 +131,7 @@ const Header = () => {
       {pop && (
         <div ref={popRef} className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-         <a href={net==`mainnet`?undefined:`https://www.mintbase.xyz/`}>  <button
+         <a href={net==`mainnet`?undefined:`${process.env.MAINNET_URL || 'https://minsta.org'}`}>  <button
               onClick={handleCloseMain}
               className="block px-4 py-2 text-sm text-gray-700 w-full text-left"
               role="menuitem"
@@ -140,7 +140,7 @@ const Header = () => {
             Mainnet
             </button>
             </a> 
-          <a href={net==`testnet`?undefined:`https://testnet.mintbase.xyz/`}>  <button
+          <a href={net==`testnet`?undefined:`${process.env.TESTNET_URL || 'https://testnet.minsta.org'}`}>  <button
               onClick={handleCloseTest}
               className="block px-4 py-2 text-sm text-gray-700 w-full text-left"
               role="menuitem"
